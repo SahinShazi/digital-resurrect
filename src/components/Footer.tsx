54964d08c45236dfa55b8889a54b26cc6354918a
SahinShazi <rksahinrone@gmail.com>
2025-10-21 06:54:33 +0600

    update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ const Footer = () => {
   const socialLinks = [
     { icon: Github, href: "https://github.com/SahinShazi", label: "GitHub" },
     { icon: Linkedin, href: "https://www.linkedin.com/in/sahin-shazi", label: "LinkedIn" },
-    { icon: Youtube, href: "https://youtube.com/@sahintechnology", label: "YouTube" },
     { icon: Twitter, href: "https://x.com/Sahin_Tech_1", label: "Twitter" },
   ];
 
